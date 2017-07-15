99593419446594970e58ab87345cafb586fb8b6d
Tomohide Ishigami <gogo5nta@gmail.com>
2017-07-15 13:03:11 +0900

    debug launch add

--- debug2.sh
+++ debug2.sh
@@ -1,2 +1,2 @@
 echo "start debug2 spawn_robot.launch"
-gnome-terminal -e "/opt/ros/indigo/bin/roslaunch turtle_war spawn_robot.launch"
+gnome-terminal -e "/opt/ros/indigo/bin/roslaunch turtle_war spawn_robot_debug.launch"
